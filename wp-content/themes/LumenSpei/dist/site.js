cadd09a3e1e396b6fcbc81fc7c5ac9b164f8d01d
Aleksandar Maravic <aleksandar.maravic@librafire.com>
2024-02-10 14:47:49 +0100

    apply small fix on mobile menu

--- wp-content/themes/LumenSpei/dist/site.js
+++ wp-content/themes/LumenSpei/dist/site.js
@@ -141,19 +141,31 @@ let _this = module.exports = {
 	-------------------------------------------------------------------------------*/
 	init: function () {
 		if( _this ){
-			$(window).on('load resize', function() {
-                if($(window).width() <= 991) {
-                    _this.$dom.menuToggle.on('click', function() {
-                        $(this).toggleClass('active');
-                        $('body').toggleClass('overflow-hidden');
-                        _this.$dom.menuContainer.toggleClass('active');
-                    });
-
-					$('.site-header-nav-menu a').on('click', function() {
-                        _this.$dom.menuToggle.removeClass('active');
-                        $('body').removeClass('overflow-hidden');
-                        _this.$dom.menuContainer.removeClass('active');
-                    });
+            function toggleMenu() {
+                _this.$dom.menuToggle.toggleClass('active');
+                $('body').toggleClass('overflow-hidden');
+                _this.$dom.menuContainer.toggleClass('active');
+            }
+        
+            _this.$dom.menuToggle.on('click', function() {
+                if ($(window).innerWidth() <= 991) {
+                    toggleMenu();
+                }
+            });
+        
+            $('.site-header-nav-menu a').on('click', function() {
+                if ($(window).innerWidth() <= 991) {
+                    _this.$dom.menuToggle.removeClass('active');
+                    $('body').removeClass('overflow-hidden');
+                    _this.$dom.menuContainer.removeClass('active');
+                }
+            });
+        
+            $(window).on('resize', function() {
+                if ($(window).innerWidth() > 991) {
+                    _this.$dom.menuToggle.removeClass('active');
+                    $('body').removeClass('overflow-hidden');
+                    _this.$dom.menuContainer.removeClass('active');
                 }
             });
 		}
